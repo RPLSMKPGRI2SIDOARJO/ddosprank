e0db2c4cc3fa4e315e660a3063d5e30fa96d9884
RPLSMEGRIDA <107708432+RPLSMKPGRI2SIDOARJO@users.noreply.github.com>
2022-06-26 02:11:41 +0700

    Add files via upload

---
+++ ddosprank.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+apt install figlet
+clear
+sleep 0.5
+figlet "WEB DDOS"
+echo "BY: X-RPL$"
+echo "Date creation: 26/06/2022"
+echo ""
+read -p "Ip web> " ip
+while :
+do
+	echo "Mengirim paket ke $ip"
+	sleep 0.1
+done
+
